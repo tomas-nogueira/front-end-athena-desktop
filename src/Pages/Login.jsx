611836e8011aadf898d97e4ded41212e8222b596
tomas-nogueira <tomasmnogueira2006@gmail.com>
2024-09-10 15:32:56 -0300

    Alterações no Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Style from '../Styles/Login.module.css'
+import Logo from '../Photos/logo_athena 1.png'
 
 function Login() {
   return (
-    <div>
-      <p>login</p>
+    <div className={Style.bg}>
+      <div className={Style.container}>
+        <img src={Logo} className={Style.logo}/>
+        <div>
+        <p>Bem vindo de volta</p>
+      </div>
+      </div>
     </div>
   )
 }
